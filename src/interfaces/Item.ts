0f58d8ff457f17f2b4d92ea8a8b72e24f3a8818d
Rodrigo Martins <rodrigo.martins7121@gmail.com>
2020-07-20 01:24:19 -0300

    Update property 'codigo' from Item interface to optional

--- src/interfaces/Item.ts
+++ src/interfaces/Item.ts
@@ -1,8 +1,10 @@
 export interface Item {
+  codigo?: string;
   produto: string;
   variedade: string;
   pais: string;
   categoria: string;
   safra: string;
   preco: number;
+  quantidadeVendas: number;
 }
