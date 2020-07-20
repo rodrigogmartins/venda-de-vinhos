d9a35a44b95fa1e4cfb2e8e63e1464adfe6a96db
Rodrigo Martins <rodrigo.martins7121@gmail.com>
2020-07-20 19:02:09 -0300

    Changing the customers stats page endpoint

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import CustomersList from "./pages/CustomersList";
+import Home from "./pages/Home";
+import CustomersStatsList from "./pages/CustomersStatsList";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<CustomersList />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/customers-stats" element={<CustomersStatsList />} />
       </Routes>
     </BrowserRouter>
   );
